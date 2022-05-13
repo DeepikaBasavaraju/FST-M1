ea6536fc5b04782d504bccba120751254143b088
Deepika B <deepikajewel@gmail.com>
2022-05-13 13:36:25 +0530

    Add files via upload

---
+++ Selenium/Activities/Activity1.java
@@ -0,0 +1,12 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Activity1 {
+    public static void main(String[] args) {
+        WebDriver driver = new FirefoxDriver();
+        driver.get("https://www.google.com/");
+        driver.manage().window().maximize();
+        driver.close();
+    }
+}
